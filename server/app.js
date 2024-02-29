02022105afc07d305a1e549d34b01663a143d092
Sara Gdbg <146811766+SaraGdbg@users.noreply.github.com>
2024-02-29 14:02:32 +0100

    🎨update chat msg time stamp

--- server/app.js
+++ server/app.js
@@ -2,28 +2,30 @@ const app = require("express")();
 const server = require("http").createServer(app);
 
 const io = require("socket.io")(server, {
-    cors: {
-        origin:"*",
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
 app.get("/test", (req, res) => {
-    res.send("<h1>Socket</h1>")
-})
+  res.send("<h1>Socket</h1>");
+});
 
 io.on("connection", (socket) => {
-    //console.log("connection", socket)
-    //socket.emit("chat", {name: "computer", message:"Hello World", timestamp: "2024"})
+  //console.log("connection", socket)
+  //socket.emit("chat", {name: "computer", message:"Hello World", timestamp: "2024"})
 
-    socket.on("chat", (arg) => {
-        let currentTime = new Date();
+  socket.on("chat", (arg) => {
+    let currentTime = new Date();
 
-        let timestamp = currentTime.toTimeString();
-        arg.timestamp = timestamp;
-        console.log("incoming chat", arg);
-        io.emit("chat", arg);
-    })
-})
+    // UPDATE: format time stamp
+    let options = { year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit" };
+    let timestamp = currentTime.toLocaleString("sv-SE", options);
+    arg.timestamp = timestamp;
+    console.log("incoming chat", arg);
+    io.emit("chat", arg);
+  });
+});
 
 server.listen(3000);
