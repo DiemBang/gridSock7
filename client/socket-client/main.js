cfb9024fb54c05683cac111b98838b714661360e
Sara Gdbg <146811766+SaraGdbg@users.noreply.github.com>
2024-02-29 13:57:34 +0100

    Test branch (do not merge)

--- client/socket-client/main.js
+++ client/socket-client/main.js
@@ -1,4 +1,4 @@
-import { io } from  "socket.io-client";
+import { io } from "socket.io-client";
 
 const socket = io("http://localhost:3000");
 
@@ -8,17 +8,29 @@ let sendBtn = document.getElementById("sendBtn");
 let chatList = document.getElementById("chatList");
 
 sendBtn.addEventListener("click", () => {
-  console.log("send chat", sendMessage.value);
-  socket.emit("chat", {name: chatName.value, message: sendMessage.value}); //arg is an object
-})
+  //  UPDATE: only updates chat if there is a name and message typed
+  if (chatName.value && sendMessage.value) {
+    console.log("send chat", sendMessage.value);
+    socket.emit("chat", { name: chatName.value, message: sendMessage.value }); //arg is an object
+    // UPDATE: clear chat entry
+    sendMessage.value = "";
+  }
+});
 
-socket.on("chat", (arg) =>{
+socket.on("chat", (arg) => {
   console.log("socket", arg);
   updateChat(arg);
-})
+});
 
 function updateChat(chat) {
-  let li = document.createElement("li"); 
-  li.innerText = chat.name + ' ' + '['+chat.timestamp+']' + ' ' +chat.message;
+  let li = document.createElement("li");
+  li.innerText = chat.name + " " + "[" + chat.timestamp + "]" + " " + chat.message;
+
+  // UPDATE: add class to msgs
+  if (chat.name === chatName.value) {
+    li.classList.add("own-msg");
+  } else {
+    li.classList.add("other-user-msg");
+  }
   chatList.appendChild(li);
 }
