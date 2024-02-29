a8f9fec6b9a1c435ec329733ae00f60798190111
helenaskagerlid <helenaskagerlid@gmail.com>
2024-02-29 14:50:43 +0100

    :sparkles: add join room feature

--- server/app.js
+++ server/app.js
@@ -12,7 +12,11 @@ app.get("/test", (req, res) => {
     res.send("<h1>Socket</h1>")
 })
 
+const mainRoom = "main";
+
 io.on("connection", (socket) => {
+    // When a user connects they enter the mainroom
+    socket.join(mainRoom);
     //console.log("connection", socket)
     //socket.emit("chat", {name: "computer", message:"Hello World", timestamp: "2024"})
 
@@ -22,8 +26,24 @@ io.on("connection", (socket) => {
         let timestamp = currentTime.toTimeString();
         arg.timestamp = timestamp;
         console.log("incoming chat", arg);
-        io.emit("chat", arg);
+        
+        io.to(mainRoom).emit("chat", arg);
+        io.to(arg.room).emit("chat", arg);
     })
+    //An eventlistener for "joinRoom" where the user exits the mainroom and joins the choosen room
+    socket.on("joinRoom", (room) => {
+        socket.leave(mainRoom);
+        socket.join(room);
+        //A message is displayed that says which room the user has entered
+        socket.emit("chat", {
+            name: "System",
+            message: `You have entered the ${room} room.`,
+            timestamp: new Date().toTimeString(),
+            room: room,
+        });
+      });
+    
+    
 })
 
 server.listen(3000);
