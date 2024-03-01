57f220cc5cd3d40f29f624cdaa912af41555fde6
Diem Bang <diembang09@gmail.com>
2024-03-01 08:58:53 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -17,9 +17,12 @@ io.on("connection", (socket) => {
     //socket.emit("chat", {name: "computer", message:"Hello World", timestamp: "2024"})
 
     socket.on("chat", (arg) => {
+        // arg should be an object with the keys name and message
+        
         let currentTime = new Date();
 
         let timestamp = currentTime.toTimeString();
+        // add timestamp key and value to arg object
         arg.timestamp = timestamp;
         console.log("incoming chat", arg);
         io.emit("chat", arg);
