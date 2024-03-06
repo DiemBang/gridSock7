467f4502a37793f64258062693e86f8de43bc0e8
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-03-06 15:16:10 +0100

    add callback function for timer when 4 players joined

--- client/socket-client/timer.js
+++ client/socket-client/timer.js
@@ -1,9 +1,7 @@
+
+// Create a callback function with seonds there the timer should start.
 export function timer(seconds, callback) {
     console.log(`timer started for ${seconds} seconds`);
-    const timer = setTimeout(() => {
-        if (callback) {
-            callback();
-        }
-    }, seconds * 1000);
+    const timer = setTimeout(callback, seconds * 1000);
     return timer;
 }
