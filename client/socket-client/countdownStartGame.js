d0d48d3167609cc4e1f1b738cd68b10125aac44f
Sara Gdbg <146811766+SaraGdbg@users.noreply.github.com>
2024-03-08 14:05:33 +0100

    💡add psuedo code

---
+++ client/socket-client/countdownStartGame.js
@@ -0,0 +1,7 @@
+// Tömma gridcontainern på första sidan så att den innehåller välkomsstext
+// + information om hur spelet går till
+// (Visa en räknare som visar hur många spelare som fattas innan spelet startar? )
+// När fyra spelare har anslutit:
+// skapa en timer som räknar ner 10-15s
+// Visa tiden som räknas ner
+// Starta funktionen printStartImage() som visar facit i några sekunder och sedan startar "målargridet"
