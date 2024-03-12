3132c746a805c6dc59d735dd03766b7571376a86
Diem Bang <diembang09@gmail.com>
2024-03-12 12:27:41 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -103,7 +103,7 @@ io.on("connection", (socket) => {
     } else {
       //if the user doesn't exist in the list the assined userId is +1 of the latest assigned userId
       userId = latestUserId++;
-      if (latestUserId === 1) {
+      if (latestUserId === 4) {
         io.emit("fourPlayersConnected");
         console.log("four user connected");
       }
